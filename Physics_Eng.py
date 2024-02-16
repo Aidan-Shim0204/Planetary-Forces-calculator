42840ddf20f342c295e7e6bc6764bdeb5738cf7f
Aidan_Shim <aidanshim0204@gmail.com>
2024-02-16 09:45:23 -0800

    Fibonacci noteboook

--- Physics_Eng.py
+++ Physics_Eng.py
@@ -2,9 +2,7 @@
 import math #For math stuff 
 import numpy as np # More math
 import matplotlib.pyplot as plt # Sketching out diagrams and models for kinematics
-from pysketcher import *
-import pandas as pd
-import openpyxl
+from pysketcher import * # Do I need dis?
 
 def main(): 
 	while True: 
@@ -15,10 +13,9 @@ def planet_physics_functions():
     # Setting up values and variables for energy, weight, and mass
     gravity = [3.59, 8.87, 9.81, 3.77, 25.95, 11.08, 10.67, 14.07]
     
-    planet_mass = []
-    planet_radius =[]
+    planet_mass = [3.3E23, 4.87E24, 5.974E24, 6.417E23, 1.898E27, 5.68E26, 8.68E25, 1.02E26]
+    planet_radius =[2439500, 6052000, 6378000, 3396000, 71492000, 60268000, 25559000, 24764000]
     G_const = 6.6743E-11
-    
     
     
     
@@ -37,9 +34,10 @@ def planet_physics_functions():
         global planet_name
         
         planet_name = planets[Planet_Choice - 1]
+        
         gravity_value = gravity[Planet_Choice - 1]
         weight_force = float(Mass_Calc * gravity_value)
-        print(str(weight_force) + "N is the weight of the mass on the planet " + planet_name)
+        print(str(weight_force) + "N is the weight of the mass on the planet on the surface [ Excluding Height ] of " + planet_name)
         
     else:
         print("This planet choice does not exist, try again")
@@ -47,16 +45,24 @@ def planet_physics_functions():
 
     # Setting up functions [For calculations]
     
+    
+    # Dynamic Gravitational Height Calculation 
+    def trueGravity():
+        global truegravVal
+        truegravVal = float(G_const * planet_mass[Planet_Choice - 1]) / (((planet_radius[Planet_Choice -1])+ height)*((planet_radius[Planet_Choice -1])+ height))
+        return truegravVal
+        
+
     #Potential Energy Value Set Up
     def potenergyCalc(Mass_Calc, gravity_value, height):
         global potential_energy
-        potential_energy = Mass_Calc * gravity_value * height
+        potential_energy = Mass_Calc * trueGravity() * height
         print(str(potential_energy) + " Joules is the potential-energy of the mass-gravitational height system")
     
     # Solving Kinematic Equations
     def kinematicCalc(gravity_value, height):
         global time
-        time = float(math.sqrt(2 * height / gravity_value))
+        time = float(math.sqrt(2 * height / trueGravity()))
         return time 
 
     
@@ -117,6 +123,9 @@ def data_store():
     Inst_PE = int
     
 # Iterates over the whole function
+
 if __name__ == '__main__': main() 
+
+
 
 
